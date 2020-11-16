61b741a5f14f8f40b3ee33eb18b3f12017d2c36b
HL9933 <834950035@qq.com>
2020-11-16 16:27:25 +0800

    修改找不到class的bug

--- src/feibonaqi.java
+++ src/feibonaqi.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
+
 public class feibonaqi {
     public static void main(String[] args) {
-        int a=1,b=1,c=0;
+        int a = 1, b = 1, c = 0;
         System.out.println("输入n");
-        Scanner sc =new Scanner(System.in);
-        int n =sc.nextInt();
-        if(n==1){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n == 1) {
             System.out.println(c);
-        }else if(n==2||n==3){
+        } else if (n == 2 || n == 3) {
             System.out.println(a);
-            }else {
+        } else {
             for (int i = 3; i <= n; i++) {
                 c = a + b;
                 a = b;
                 b = c;
             }
             System.out.println(c);
-            }
         }
     }
 }
